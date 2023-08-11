726e3123478e635c913fa31c4041752bd5d97d27
ayazukuri <korscr1027@yahoo.com>
2023-08-12 01:28:16 +0200

    feat: channel list, fix formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-import './App.css'
-import MemberList from './components/MemberList'
+import './App.css';
+import ChannelList from './components/ChannelList';
+import MemberList from './components/MemberList';
+
+const dummyMembers = [
+  { id: "0", name: "CoolKind" },
+  { id: "1", name: "RadiergummiHD" }
+];
+
+const dummyChannels = [
+  { id: "2", name: "channel1" },
+  { id: "3", name: "channel2" }
+];
 
 function App() {
   return (
@@ -7,9 +18,9 @@ function App() {
       <div className="sidebar"></div>
       <div className="groupinfo"></div>
       <div className="title"></div>
-      <div className="channels"></div>
+      <div className="channels"><ChannelList channels={dummyChannels} /></div>
       <div className="chat"></div>
-      <div className="members"><MemberList /></div>
+      <div className="members"><MemberList members={dummyMembers} /></div>
       <div className="userinfo"></div>
       <div className="input"></div>
       <div className="settings"></div>
---
+++ src/components/ChannelEntry.tsx
@@ -0,0 +1,7 @@
+import "./ChannelEntry.css";
+
+export default function ChannelEntry(props: { key: string, name: string }) {
+    return <div channel-id={props.key} className="channel-entry">
+        {props.name}
+    </div>
+}
