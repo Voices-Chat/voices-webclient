eb8f53f87d89228cdd3eaf6412040f7a9bd1af81
Toprak Saricerci <toprak.saricerci@gmail.com>
2023-08-13 10:22:59 +0200

    refactor: extracted scrollbar css-properties

--- src/components/MemberList.tsx
+++ src/components/MemberList.tsx
@@ -1,8 +1,9 @@
 import "./MemberList.css";
+import "./scrollable.css";
 import MemberEntry from "./MemberEntry";
 
 export default function MemberList(props: { members: { id: string, name: string }[] }) {
-    return <div className="member-list">
+    return <div className="member-list scrollable">
         <span className="member-list-title">Members</span>
         {props.members.map((mem) => <MemberEntry key={mem.id} name={mem.name} />)}
     </div>;
