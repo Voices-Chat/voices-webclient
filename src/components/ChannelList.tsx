bb4985db0680bfe7b0afeb392b3e0fd5ffcc6f82
Toprak Saricerci <toprak.saricerci@gmail.com>
2023-08-13 14:13:48 +0200

    feat: channel-list scrollbar

--- src/components/ChannelList.tsx
+++ src/components/ChannelList.tsx
@@ -1,8 +1,9 @@
 import ChannelEntry from "./ChannelEntry";
 import "./ChannelList.css";
+import "./scrollable.css";
 
 export default function ChannelList(props: { channels: { id: string, name: string }[] }) {
-    return <div className="channel-list">
+    return <div className="channel-list scrollable">
         {props.channels.map((ch) => <ChannelEntry key={ch.id} name={ch.name} />)}
     </div>;
 }
