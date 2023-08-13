9f3f214ad7cf9c4567d9d84bc3cc5203f34504cd
Toprak Saricerci <toprak.saricerci@gmail.com>
2023-08-13 13:54:53 +0200

    fix: ellipsis for text-overflow

--- src/components/MemberEntry.tsx
+++ src/components/MemberEntry.tsx
@@ -2,6 +2,8 @@ import "./MemberEntry.css";
 
 export default function MemberEntry(props: { key: string, name: string }) {
     return <div member-id={props.key} className="member-entry">
-        {props.name}
+        <div className="member-name">
+            {props.name}
+        </div>
     </div>;
 }
