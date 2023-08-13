6e5e879496795bca6eb55e97381458c60faf3adc
Toprak Saricerci <toprak.saricerci@gmail.com>
2023-08-13 13:55:22 +0200

    style: consistent naming

--- src/components/MessageEntry.tsx
+++ src/components/MessageEntry.tsx
@@ -2,7 +2,7 @@ import "./MessageEntry.css";
 
 export default function MessageEntry(props: { key: string, authorname: string, authorid: string, creationdate: Date, content: string }) {
     return <div message-id={props.key} author-id={props.authorid} className="message-entry">
-        <div className="msg-author">{props.authorname} ・ {props.creationdate.toLocaleDateString("de")}</div>
-        <div className="msg-text">{props.content}</div>
+        <div className="message-author">{props.authorname} ・ {props.creationdate.toLocaleDateString("de")}</div>
+        <div className="message-text">{props.content}</div>
     </div>;
 }
